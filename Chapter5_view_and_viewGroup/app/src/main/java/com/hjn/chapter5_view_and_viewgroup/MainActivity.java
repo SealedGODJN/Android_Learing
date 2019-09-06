89a0d1004675eb5309c1cf8ab14714c8b8f35c1e
hjn <1286039722@qq.com>
2019-09-06 14:48:14 +0800

    Chapter5：视图和视图组 利用（android:layout_weight)为多个视图增加权重，改变各个视图占据的空间

---
+++ Chapter5_view_and_viewGroup/app/src/main/java/com/hjn/chapter5_view_and_viewgroup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hjn.chapter5_view_and_viewgroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
