ffc1d33f71fe07894b6ffdd7947ccd49c4271685
hjn <1286039722@qq.com>
2019-09-09 14:48:19 +0800

    Chapter8：支持库和应用条 了解如何增加应用条

---
+++ Chapter8_support_lib_and_application_bar/app/src/main/java/com/hjn/chapter8_support_lib_and_application_bar/OrderActivity.java
@@ -0,0 +1,18 @@
+package com.hjn.chapter8_support_lib_and_application_bar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
