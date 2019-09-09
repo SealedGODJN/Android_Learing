151a23924bdbaa38ef528382069bf22e271506f9
hjn <1286039722@qq.com>
2019-09-09 22:13:05 +0800

    Chapter9：片段 了解片段管理器，片段和视图的不同，片段的生命周期及其方法；学会有条件的调用片段

--- Chapter9_fragment/app/src/main/java/com/hjn/chapter9_fragment/DetailActivity.java
+++ Chapter9_fragment/app/src/main/java/com/hjn/chapter9_fragment/DetailActivity.java
@@ -9,6 +9,12 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 在执行下面这条语句之前，片段还没有创建
         setContentView(R.layout.activity_detail);
+
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        // 测试片段能否正常工作
+        frag.setWorkoutId(1);
     }
 }
--- Chapter9_fragment/app/src/main/java/com/hjn/chapter9_fragment/WorkoutDetailFragment.java
+++ Chapter9_fragment/app/src/main/java/com/hjn/chapter9_fragment/WorkoutDetailFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +16,12 @@ import android.view.ViewGroup;
  */
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workoutId;
+
+    // 活动使用该方法设置训练项目ID的值
+    public void setWorkoutId(long id) {
+        this.workoutId = id;
+    }
 
     public WorkoutDetailFragment() {
         // Required empty public constructor
@@ -26,6 +33,21 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 得到片段的根视图，然后使用根视图得到两个文本视图
+        View view = getView();
+        if(view != null) {
+            // 获得workoutId对应的workout项
+            Workout workout = Workout.workouts[(int) workoutId];
+            TextView title = (TextView)view.findViewById(R.id.textTitle);
+            title.setText(workout.getName());
+            TextView description = (TextView)view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
 }
