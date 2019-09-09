dbe4d45d78c62dcc22bda82cef175e38fbe31bbe
hjn <1286039722@qq.com>
2019-09-09 22:50:44 +0800

    Chapter9：片段 了解列表片段，实现在MainActivity中显示列表片段；未实现点击列表项的交互

---
+++ Chapter9_fragment/app/src/main/java/com/hjn/chapter9_fragment/WorkoutListFragment.java
@@ -0,0 +1,42 @@
+package com.hjn.chapter9_fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutListFragment extends ListFragment {
+
+
+    public WorkoutListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(), android.R.layout.simple_list_item_1,
+                names
+        );
+        // 将数组适配器绑定到列表视图
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
