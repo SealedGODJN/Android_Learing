98227dd5c7e081c79558c4c4dcb2e5fa35642c16
hjn <1286039722@qq.com>
2019-11-01 02:16:43 +0800

    2019.11.1 Chapter12：完成多个页面的滑动切换，找到了AppBarLayout所属的包
    待完成：
    1、为MainActivity增加标签页导航

--- Chapter8_support_lib_and_application_bar/app/src/main/java/com/hjn/chapter8_support_lib_and_application_bar/MainActivity.java
+++ Chapter8_support_lib_and_application_bar/app/src/main/java/com/hjn/chapter8_support_lib_and_application_bar/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // 让视图分页控件关联到片段分页控件适配器
+        SectionsPagerAdapter pagerAdapter =
+                new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
     }
 
     @Override
@@ -56,6 +66,42 @@ public class MainActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         *
+         * @param Position 当前页面的标号
+         * @return 指定各个页面应当显示哪个片段
+         */
+        @Override
+        public Fragment getItem(int Position){
+            switch (Position) {
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoresFragment();
+            }
+            return null;
+        }
+
+        /**
+         *
+         * @return 片段的数量
+         */
+        @Override
+        public int getCount() {
+            return 4;
         }
     }
 }
---
+++ Chapter8_support_lib_and_application_bar/app/src/main/java/com/hjn/chapter8_support_lib_and_application_bar/PizzaFragment.java
@@ -0,0 +1,31 @@
+package com.hjn.chapter8_support_lib_and_application_bar;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class PizzaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pizzas));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater,container,savedInstanceState);
+    }
+
+}
